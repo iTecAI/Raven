39815c24623d9443e494040e3fc3d7102dd2f041
Dax Harris <mvh3875@rit.edu>
2024-07-21 13:53:35 -0400

    remove old custom resource defs

--- raven_api/common/models/__init__.py
+++ raven_api/common/models/__init__.py
@@ -1,6 +1,5 @@
 from .config import Config
 from .auth import Session, User, AuthState, RedactedUser, EventContext, glob_match
 from .scope import Scope, CORE_SCOPE
-from .custom_resource import CustomResource, CustomResourceExecutor
 
-DOCUMENT_MODELS = [Session, User, EventContext, CustomResource]
+DOCUMENT_MODELS = [Session, User, EventContext]

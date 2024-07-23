8d6471b4dc6243b8891e7cd4067d7bdccf09d3c1
Dax Harris <mvh3875@rit.edu>
2024-07-23 15:23:53 -0400

    string input default value

--- raven_client/src/views/pipelines/io/fields/stringField.tsx
+++ raven_client/src/views/pipelines/io/fields/stringField.tsx
@@ -1,15 +1,28 @@
-import { Group, NumberInput, Stack, Switch, Text } from "@mantine/core";
+import {
+    Group,
+    NumberInput,
+    Stack,
+    Switch,
+    Text,
+    Textarea,
+    TextInput,
+} from "@mantine/core";
 import { StringField } from "../../../../types/backend/pipeline";
 import { FieldCreationProps, IOFieldRenderer } from "./types";
 import { MetaFields } from "./util";
 import { useTranslation } from "react-i18next";
-import { IconLetterCase, IconRuler2, IconTextWrap } from "@tabler/icons-react";
+import {
+    IconForms,
+    IconLetterCase,
+    IconRuler2,
+    IconTextWrap,
+} from "@tabler/icons-react";
 import { isNumber } from "lodash";
 
 function StringFieldEditor(props: FieldCreationProps<StringField>) {
     const { t } = useTranslation();
     return (
-        <Stack gap="sm">
+        <Stack gap="xs">
             <Group gap="sm">
                 <IconLetterCase />
                 <Text>{t("util.pipelines.io.field.string.title")}</Text>
@@ -19,6 +32,27 @@ function StringFieldEditor(props: FieldCreationProps<StringField>) {
                 setValue={props.setValue}
                 fields={props.fields}
             />
+            {props.value.multiline ? (
+                <Textarea
+                    leftSection={<IconForms size={20} />}
+                    label={t("util.pipelines.io.meta.default")}
+                    value={props.value.default_value ?? ""}
+                    onChange={(v) =>
+                        props.setValue({ default_value: v.target.value })
+                    }
+                    autosize
+                    maxRows={4}
+                />
+            ) : (
+                <TextInput
+                    leftSection={<IconForms size={20} />}
+                    label={t("util.pipelines.io.meta.default")}
+                    value={props.value.default_value ?? ""}
+                    onChange={(v) =>
+                        props.setValue({ default_value: v.target.value })
+                    }
+                />
+            )}
             <NumberInput
                 leftSection={<IconRuler2 size={20} />}
                 allowDecimal={false}
@@ -31,6 +65,7 @@ function StringFieldEditor(props: FieldCreationProps<StringField>) {
                 }
                 label={t("util.pipelines.io.field.string.max_length")}
             />
+
             <Group gap="sm" justify="space-between">
                 <Group gap="sm">
                     <IconTextWrap />
